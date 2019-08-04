422cb00cf92b75ec3ad8df7291f5c98d128794f9
Maciek Debski <maciej.debski@g2.pl>
2019-08-04 16:08:13 +0200

    Initial commit

---
+++ src/main/java/com/example/bookshelf/storage/impl/StaticListBookStorageImpl.java
@@ -0,0 +1,27 @@
+package com.example.bookshelf.storage.impl;
+
+import com.example.bookshelf.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticListBookStorageImpl {
+
+    private static List<Book> bookStorage = new ArrayList<Book>();
+
+    public Book getBook(long id){
+        for (Book book: bookStorage){
+            if (book.getId() == id){
+                return book;
+            }
+        }return null;
+    }
+
+    public  List<Book> getAllBooks(){
+        return bookStorage;
+    }
+
+    public void addBook (Book book){
+        bookStorage.add(book);
+    }
+}
